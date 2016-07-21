47c7771aaae6c505aba1caeca4047696c244144d
Vitaliy Zhukov <DragonZX@yandex.ru>
2016-07-21 12:35:58 +0300

    Manifest & info fix

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -1,4 +1,5 @@
-﻿Imports System
+﻿Imports System.Resources
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -8,17 +9,17 @@ Imports System.Runtime.InteropServices
 
 ' Rivedere i valori degli attributi dell'assembly
 
-<Assembly: AssemblyTitle("DriverBackup! 2")> 
-<Assembly: AssemblyDescription("Drivers' backup & restore tool")> 
-<Assembly: AssemblyCompany("Giuseppe Greco")> 
-<Assembly: AssemblyProduct("DriverBackup! 2")> 
-<Assembly: AssemblyCopyright("GPL License 2009-2010")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("DriverBackup! 2")>
+<Assembly: AssemblyDescription("Drivers' backup & restore tool")>
+<Assembly: AssemblyCompany("Giuseppe Greco")>
+<Assembly: AssemblyProduct("DriverBackup! 2")>
+<Assembly: AssemblyCopyright("GPL License 2009-2016")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'Se il progetto viene esposto a COM, il GUID che segue verrà utilizzato per creare l'ID della libreria dei tipi
-<Assembly: Guid("8918dea9-f5a8-4265-a622-6040796350ee")> 
+<Assembly: Guid("8918dea9-f5a8-4265-a622-6040796350ee")>
 
 ' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
 '
@@ -31,5 +32,6 @@ Imports System.Runtime.InteropServices
 ' utilizzando l'asterisco (*) come descritto di seguito:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.1.0.0")> 
-<Assembly: AssemblyFileVersion("2.1.0.4")> 
+<Assembly: AssemblyVersion("2.2.0.0")>
+<Assembly: AssemblyFileVersion("2.2.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
